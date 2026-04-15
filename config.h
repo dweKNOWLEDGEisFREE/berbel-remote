8d19539c7170aed7e527947c686a1c05487ea652
KNOWLEDGEisFREE <42681617+dweKNOWLEDGEisFREE@users.noreply.github.com>
2026-04-15 10:47:18 +0200

    Add files via upload

--- config.h
+++ config.h
@@ -1,17 +1,3 @@
 #pragma once
-
-// WiFi
-#define WIFI_SSID "WIFI-NET"
-#define WIFI_PASS "WIFI-PASS"
-
-// MQTT
-#define MQTT_HOST "192.168.xxx.xxx"
-#define MQTT_PORT 1883
-#define MQTT_USER "mqtt-user"
-#define MQTT_PASS "mqtt-pass"
-
-// Hood features
-// Set to false if your hood has no retractable cover (lift function).
-// When false, Position, Hochfahren, Herunterfahren, and Cover State
-// entities will not be created in Home Assistant.
-#define HOOD_HAS_COVER true
+// Konfiguration wird beim ersten Start über den Setup-Wizard eingegeben
+// und im NVS (Flash) gespeichert. Diese Datei wird nicht mehr benötigt.
